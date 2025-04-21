9da8aa05a8d78accf2d05ea1a483d695fab048f4
UMANG-2003 <umangkumar20003@gmail.com>
2025-04-21 12:20:33 +0530

    #

--- app/page.js
+++ app/page.js
@@ -1,103 +1,113 @@
-import Image from "next/image";
+"use client";
+import React, { useState, useEffect } from "react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [data, setData] = useState([]);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+  useEffect(() => {
+    const storedData = localStorage.getItem("users");
+    if (storedData) {
+      setData(JSON.parse(storedData));
+    }
+  }, []);
+
+  const addUser = (name, age) => {
+    const id = data.length + 1;
+    const newData = [...data, { id, name, age }];
+    setData(newData);
+
+    localStorage.setItem("users", JSON.stringify(newData));
+  };
+  const deleteuser = (ids) => {
+    const parsedId = parseInt(ids, 10);
+    setData(data.filter((user) => user.id !== parsedId));
+  };
+
+  return (
+    <>
+      <h1 className="text-center my-4 text-3xl font-bold underline">
+        User Registration System
+      </h1>
+
+      <div className="w-[80%] mx-auto font-bold underline">
+        <h1 className="text-xl">User Register</h1>
+        <form
+          className="my-4"
+          onSubmit={(e) => {
+            e.preventDefault();
+            const name = e.target.elements.name.value;
+            const age = e.target.elements.age.value;
+            addUser(name, age);
+            e.target.reset();
+          }}
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+          <label htmlFor="name">Name</label>
+          <input
+            className="bg-gray-500 mx-4 p-1"
+            type="text"
+            name="name"
+            id="name"
+            required
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          <label htmlFor="age">Age</label>
+          <input
+            className="bg-gray-500 mx-4 p-1"
+            type="number"
+            name="age"
+            id="age"
+            required
+          />
+          <button type="submit">Register</button>
+        </form>
+      </div>
+
+      <div className="w-[80%] mx-auto my-20">
+        <h2 className="text-xl font-bold">Delete Users:</h2>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            const ids = e.target.elements.ids.value;
+            deleteuser(ids);
+            e.target.reset();
+          }}
         >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
+          <input
+            type="number"
+            name="ids"
+            className="bg-gray-500 mx-4 p-1"
+            required
           />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+          <button type="submit">Delete user</button>
+        </form>
+      </div>
+
+      <div className="w-[80%] mx-auto mt-4">
+        <h2 className="text-xl font-bold">Registered Users:</h2>
+        <table className="table-auto w-full border-collapse border border-gray-500">
+          <thead>
+            <tr>
+              <th className="border border-gray-500 px-4 py-2">ID</th>
+              <th className="border border-gray-500 px-4 py-2">Name</th>
+              <th className="border border-gray-500 px-4 py-2">Age</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((user, index) => (
+              <tr key={index}>
+                <td className="border border-gray-500 px-4 py-2 text-center">
+                  {user.id}
+                </td>
+                <td className="border border-gray-500 px-4 py-2 text-center">
+                  {user.name}
+                </td>
+                <td className="border border-gray-500 px-4 py-2 text-center">
+                  {user.age}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
   );
 }
